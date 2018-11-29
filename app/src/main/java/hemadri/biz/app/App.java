1e925739ecdddd2ff3f019f1ded6cc2e4591fa48
Murugan <velsan@bizruntime.com>
2018-11-29 20:36:15 +0530

    ignite

---
+++ app/src/main/java/hemadri/biz/app/App.java
@@ -0,0 +1,13 @@
+package hemadri.biz.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
